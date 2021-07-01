4be643a9dc371629fca289744001cdb8d1d558c0
Soneil Pitamber <pitamber@att.net>
2021-06-30 21:02:57 -0400

    Add project files.

---
+++ GeneralStoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using GeneralStoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {   
+        private ApplicationDbContext _db = new ApplicationDbContext();// Link to database
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(_db.Products.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
